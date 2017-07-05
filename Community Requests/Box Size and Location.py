ec45406141ee9a5b6f969777133f0128e951d738
gentlegiantJGC <james_clare1@yahoo.co.uk>
2017-07-05 14:28:16 +0100

    Revert "Initial move from mediafire"
    This reverts commit 4f297ebd4a5b60edf63e424cc85a43780de94824.

--- Community Requests/Box Size and Location.py
+++
@@ -1,5 +0,0 @@
-displayName = "Box Size and Location"
-
-def perform(level, box, options):
-    print str(box.minx)+ " " +str(box.miny)+ " " +str(box.minz)+ " " +str(box.maxx-1)+ " " +str(box.maxy-1)+ " " +str(box.maxz-1)
-	print "[x=" +str(box.minx)+ ",y=" +str(box.miny)+ ",z=" +str(box.minz)+ ",dx=" +str(box.maxx-box.minx-1)+ ",dy=" +str(box.maxy-box.miny-1)+ ",dz=" +str(box.maxz-box.minz-1)+ "]"
