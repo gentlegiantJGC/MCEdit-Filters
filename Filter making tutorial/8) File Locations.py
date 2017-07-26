af3e4493489c4c9d6f1b3bc60c9b6d919ad33277
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2017-07-26 09:59:40 +0200

    Update 8) File Locations.py
    Comments

--- Filter making tutorial/8) File Locations.py
+++ Filter making tutorial/8) File Locations.py
@@ -1,5 +1,7 @@
 import directories
 directories.getFiltersDir() #will return the file location of the filters directory
 directories.getMinecraftSaveFileDir() #world file should be in here (need to find the folders name though)
+'''
 Source:
 http://www.mcedit-unified.net/docs/directories.html
+'''
