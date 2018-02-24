f642dbaa3258c389157c8f117bf19a3ea3ad4d69
gentlegiantJGC <james_clare1@yahoo.co.uk>
2018-02-24 10:33:52 +0000

    Copyright notice and a number of changes
    Added copyright notice as is written at the start of the file
    Rewrote most of the filter to make adding new values much easier
    Added a lot of new values
    Added option to log errors so user can manually debug as well as automatically fixing them

--- Bedrock Edition Filters/bedrockFix.py
+++ Bedrock Edition Filters/bedrockFix.py
@@ -2,7 +2,11 @@
 #http://youtube.com/gentlegiantJGC
 #https://twitter.com/gentlegiantJGC
 
-# if you are going to use the code in this filter please give credit
+'''
+Copyright Notice
+I hereby give the user full rights to use this program as provided with the condition that it is only used for non-commercial projects. If the user would like to use this program for commercial projects an alternative can be provided without this limitation for a fee. If interested, please contact me via direct message on twitter (link above) or by other means.
+This program can be modified however this copyright notice and the creator information (lines 1-9) must remain unchanged and unmoved. This program must also not be redistributed or rehosted in any way.
+'''
 
 from pymclevel import TAG_Int, TAG_String, TAG_Compound
 import numpy as np
@@ -10,11 +14,50 @@ import random
 
 displayName = "Bedrock Fix"
 
-inputs = ()
+inputs = (
+	("Log Errors", (True)),
+	("Fix Errors", (True)),
+	("Chest Contents", (False)),
+	("Chest Loot Table", ("string", "value=loot_tables/chests/woodland_mansion.json")),
+	("Percentage of chests to populate", (100, 0, 100)),
+	("Spawner Entity", (False)),
+)
 	
 def perform(level, box, options):
 	if level.gamePlatform != 'PE':
 		raise Exception('Must be a PE/Win10... world')
+		
+	chestProb = [True]*options["Percentage of chests to populate"] + [False] * (100-options["Percentage of chests to populate"])
+	blocksSupported = {
+		23 : "Dispenser",
+		25 : "Music",
+		26 : "Bed",
+		29 : "PistonArm",
+		33 : "PistonArm",
+		52 : "MobSpawner",
+		54 : "Chest",
+		61 : "Furnace",
+		63 : "Sign",
+		84 : "Jukebox",
+		116 : "EnchantTable",
+		117 : "BrewingStand",
+		118 : "Cauldron",
+		125 : "Dropper",
+		137 : "CommandBlock",
+		130 : "EnderChest",
+		138 : "Beacon",
+		140 : "FlowerPot",
+		144 : "Skull",
+		146 : "Chest",
+		149 : "Comparator",
+		151 : "DaylightDetector",
+		154 : "Hopper",
+		176 : "Banner",
+		177 : "Banner",
+		178 : "DaylightDetector",
+		188 : "CommandBlock",
+		189 : "CommandBlock"
+	}
 
 
 	for cx, cz in box.chunkPositions:
@@ -24,72 +67,50 @@ def perform(level, box, options):
 			continue
 		chunkBlockList = np.unique(chunk.Blocks)
 		for block in chunkBlockList:
-			if block not in [52,54,116,118]:
+			if block not in blocksSupported:
 				continue
 			for coord in np.argwhere(chunk.Blocks == block):
 				x,z,y = coord
 				x += cx * 16
 				z += cz * 16
+				if (x,y,z) not in box:
+					continue
 				# get the tile entity
 				te = level.tileEntityAt(x,y,z)
+				createTileEntity = False
 				if te is None:
+					createTileEntity = True
 					#create tile entitiy
 					te = TAG_Compound()
 					te['x'] = TAG_Int(x)
 					te['y'] = TAG_Int(y)
 					te['z'] = TAG_Int(z)
+					
+					
+				if options["Log Errors"]:
+					if 'id' in te:
+						if te['id'].value != blocksSupported[block]:
+							print 'Block at {} is type "{}" but set as type "{}"'.format((x,y,z), blocksSupported[block], te['id'].value)
+					else:
+						if createTileEntity:
+							print 'Block at {} is type "{}" but has no tile entity'.format((x,y,z), blocksSupported[block])
+						else:
+							print 'Block at {} is type "{}" but has no "id" key'.format((x,y,z), blocksSupported[block])
+					
+				if options["Fix Errors"]:
+					te['id'] = TAG_String(blocksSupported[block])
+					
 					if block == 52:
-						te['id'] = TAG_String("MobSpawner")
-						te['EntityId'] = TAG_Int(random.choice([32,33,34,35]))
-						# need to randomly choose between a few
-					elif block == 54:
-						te['id'] = TAG_String("Chest")
-						te['LootTable'] = TAG_String("loot_tables/chests/woodland_mansion.json")
-					elif block == 116:
-						te['id'] = TAG_String("EnchantTable")
-					elif block == 118:
-						te['id'] = TAG_String("Cauldron")
-					chunk.TileEntities.append(te)
-				else:
-					if block == 52:
-						te['id'] = TAG_String("MobSpawner")
-						if 'EntityId' not in te or ('EntityId' in te and te['EntityId'].value == 1):
-							te['EntityId'] = TAG_Int(random.choice([32,33,34,35]))
-					elif block == 54:
-						te['id'] = TAG_String("Chest")
+						if options["Spawner Entity"]:
+							if 'EntityId' not in te or ('EntityId' in te and te['EntityId'].value == 1):
+									te['EntityId'] = TAG_Int(random.choice([32,33,34,35]))
+					elif block in [54,146]:
 						if 'Items' in te and len(te['Items']) > 0:
 							pass
-						elif 'LootTable' not in te:
-							te['LootTable'] = TAG_String("loot_tables/chests/woodland_mansion.json")
-					elif block == 116:
-						te['id'] = TAG_String("EnchantTable")
-					elif block == 118:
-						te['id'] = TAG_String("Cauldron")
+						elif 'LootTable' not in te and options["Chest Contents"] and random.choice(chestProb):
+							te['LootTable'] = TAG_String(options["Chest Loot Table"])
 					
-		chunk.chunkChanged()
-
-
-# 52
-# {
-	# EntityId: 35,
-	# id: "MobSpawner",
-# }
-
-
-# 54
-# {
-	# id: "Chest"
-	# LootTable: "loot_tables/chests/woodland_mansion.json"
-# }
-
-
-# 116
-# {
-	# id: "EnchantTable",
-# }
-
-
-# 118
-# {
-	# id: "Cauldron"
-# }
+					if createTileEntity:
+						chunk.TileEntities.append(te)
+		if options["Fix Errors"]:
+			chunk.chunkChanged()
