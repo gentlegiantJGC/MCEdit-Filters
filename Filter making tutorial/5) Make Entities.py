dd3af933a312e07abe3395d0051171aef81ecc3c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2018-03-12 20:05:11 +0000

    fixed formatting

--- Filter making tutorial/5) Make Entities.py
+++ Filter making tutorial/5) Make Entities.py
@@ -12,7 +12,7 @@ from pymclevel import TAG_Compound, TAG_Int, TAG_Short, TAG_Byte, TAG_String, TA
 
 	e = TAG_Compound()						# to create an entity you first of all need to make a TAG_compound
 	e["id"] = TAG_String("zombie")		    # You can then specify values in the compound
-    e["Pos"] = TAG_List()
+	e["Pos"] = TAG_List()
 	e["Pos"].append(TAG_Double(x)			# The part on the left is the name of the tag in the compound
 	e["Pos"].append(TAG_Double(y)			# The right part is the data type and the new value
 	e["Pos"].append(TAG_Double(z)			# For a string or a fixed value the value needs to be in quotes
@@ -20,7 +20,7 @@ from pymclevel import TAG_Compound, TAG_Int, TAG_Short, TAG_Byte, TAG_String, TA
 											# The data type must be correct. The correct type can be found on the chunk format wiki page
 											# http://minecraft.gamepedia.com/Chunk_format
 
-    chunk = level.getChunk(x/16, z/16)  # Chunk is not defined yet to this defines it
+	chunk = level.getChunk(x/16, z/16)  # Chunk is not defined yet to this defines it
 
 	chunk.Entities.append(e)		# You then need to append the compound tag to the world
 	chunk.dirty = True				# And tell MCedit that the chunk has been updated
